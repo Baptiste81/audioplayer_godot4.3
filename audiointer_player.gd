3caa9b0d37ded0b4c52a9f1a0e5b4334ebd9a44b
Baptiste81 <bpt.dapy@gmail.com>
2024-08-01 01:50:44 +0200

    Prevent crash

--- audiointer_player.gd
+++ audiointer_player.gd
@@ -2,12 +2,15 @@ class_name AudioInterPlayer extends AudioStreamPlayer
 
 
 func _on_full_pressed() -> void:
-    get_stream_playback().switch_to_clip(0)
+    if has_stream_playback():
+        get_stream_playback().switch_to_clip(0)
 
 
 func _on_drumless_pressed() -> void:
-    get_stream_playback().switch_to_clip(1)
+    if has_stream_playback():
+        get_stream_playback().switch_to_clip(1)
 
 
 func _on_guitarless_pressed() -> void:
-    get_stream_playback().switch_to_clip(2)
+    if has_stream_playback():
+        get_stream_playback().switch_to_clip(2)
